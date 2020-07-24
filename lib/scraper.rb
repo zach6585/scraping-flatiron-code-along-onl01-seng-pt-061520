d04d6a1a3f5759b157be77836996b55fe9f3e73f
zachary mines <zmines@wesleyan.edu>
2020-07-24 21:38:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,7 +37,8 @@ class Scraper
   def make_courses 
     self.get_courses.each do |post|
       course = Course.new 
-      course.title = post.css("h2").text 
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end 
   end 
